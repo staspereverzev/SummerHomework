4432a12b5f5372b5173b34d9afc6ef36d3100d99
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-06 11:42:11 +0300

    First Seminar completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int num1, num2, quad;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if(num1 == quad)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+int num, current;
+
+Console.Write("Input first integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++; //current=current + 1;
+}
+
+
+
+
