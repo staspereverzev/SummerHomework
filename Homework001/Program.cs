5dd90940c182604f0ad2cbdaa7752f3c066e9857
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-07 23:26:42 +0300

    init commit

---
+++ Homework001/Program.cs
@@ -0,0 +1,12 @@
+﻿int num, current;
+
+Console.Write("Input first integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++; //current=current + 1;
+}
