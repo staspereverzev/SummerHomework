713b7c5eda3a6bd601fa89e9cf385e1e09ba3322
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-08 21:01:30 +0300

    First task

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,12 +1,17 @@
-﻿int num, current;
+﻿int num1, num2;
 
 Console.Write("Input first integer number: ");
-num = Convert.ToInt32(Console.ReadLine());
+num1 = Convert.ToInt32(Console.ReadLine());
 
-current = num * (-1);
+Console.Write("Input first integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
 
-while(current <= num)
+if (num1 > num2)
 {
-    Console.Write(current + " ");
-    current++; //current=current + 1;
+    Console.WriteLine("First Integer number bigger then second");
 }
+else
+{
+    Console.WriteLine("Second Integer number bigger then first"); 
+}
+
