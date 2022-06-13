b3f3bf530a564b4f91926946b00eeff240287f76
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-13 10:00:34 +0300

    seminar ended

---
+++ Seminar2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+int number = new Random().Next(10,100);
+int max = int.MinValue;
+
+Console.WriteLine(number);
+
+int FirstDigit = number/10;
+int secondDigit = number % 10;
+
+if (FirstDigit > max)
+{
+    max = FirstDigit;
+}
+if (secondDigit > max)
+{
+    max = secondDigit;
+}
+
+Console.WriteLine(max);
+*/
+
+/*
+int number = new Random().Next(10,100);
+int max = int.MinValue;
+
+Console.WriteLine(number);
+
+while (number > 0)
+{
+    int lastDigit = number = number % 10;
+    if (lastDigit > max)
+    {
+        max = lastDigit;
+    }
+    number = number / 10;
+}
+
+
+Console.WriteLine(max);
+*/
+
+/*int numberMin = 10;
+int numberMax = 100;
+int imputnumber = Convert.ToInt32(Console.ReadLine());
+
+if (imputnumber > numberMin && imputnumber < numberMax)
+{
+    Console.WriteLine("Yse");
+}
+*/
+
