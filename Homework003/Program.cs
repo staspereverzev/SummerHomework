fd9706bb93ad604070c4ecb54cd7b4f41f3a399d
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-16 01:02:41 +0300

    third task

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -1,4 +1,4 @@
-﻿void Polindrom (int N, int t)
+﻿/*void Polindrom (int N, int t)
 {
     t = t % 10000;
     t = t / 10;
@@ -18,4 +18,25 @@ int t = N;
 
 
 Polindrom (N, t);
+*/
 
+/*
+void CubedN (int N)
+{
+    int count = 0;
+    int cube = 0;
+
+    while (count <= N)
+    {
+        cube = count*count*count;
+        count++;
+        Console.WriteLine(cube);
+    }
+}
+
+int N;
+Console.Write("Input integer number: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+CubedN(N);
+*/
