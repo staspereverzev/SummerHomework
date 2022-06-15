2124904cb7e65a2d75abfae1a04be40540963b1c
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-16 00:54:42 +0300

    First task

---
+++ Homework003/Program.cs
@@ -0,0 +1,21 @@
+﻿void Polindrom (int N, int t)
+{
+    t = t % 10000;
+    t = t / 10;
+    if (N / 10000 == N % 10 && t / 100 == t % 10)
+    {
+        Console.WriteLine("Palindrom");
+    }
+    else
+    {
+        Console.WriteLine("Ne palindrom");
+    }  
+}
+
+Console.WriteLine("Enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+int t = N;
+
+
+Polindrom (N, t);
+
