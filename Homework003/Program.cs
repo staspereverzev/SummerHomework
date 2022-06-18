ce3ee4c0d15f9914e7d03241f60aa1687bf4065a
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-18 15:13:20 +0300

    homework finished

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -40,3 +40,31 @@ N = Convert.ToInt32(Console.ReadLine());
 
 CubedN(N);
 */
+
+double xA; double yA; double zA; double xB; double yB; double zB;
+
+Console.Write("Введите координату X точки A: ");
+xA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y точки A: ");
+yA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z точки A: ");
+zA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату X точки B: ");
+xB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y точки B: ");
+yB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z точки B: ");
+zB = Convert.ToDouble(Console.ReadLine());
+
+double FindLenght(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    return Math.Sqrt((xB - xA)*(xB - xA) + Math.Pow((yB-yA),2) + Math.Pow((zB-zA),2));
+}
+
+double result = FindLenght(xA, yA, zA, xB, yB, zB);
+Console.WriteLine(result);
