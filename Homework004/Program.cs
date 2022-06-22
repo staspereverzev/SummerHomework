585040ee3269dbf32b4566432c096e7c4e0b2861
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-22 13:03:26 +0300

    finished

---
+++ Homework004/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Input integer number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input integer number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int toDegree (int B, int A, int result)
+{
+    int count = 1;
+        while (count < B)
+    {
+        result = result * A;
+        count++;
+    }
+return result;
+}
+int result = A;
+result = toDegree(B, A, result);
+Console.WriteLine(A + " to degree " + B + " = " + result);
+
+/*
+int size = 8;
+int[] array = new int [size];
+
+for (int i= 0; i < size; i++)
+{
+    array[i] = new Random().Next(0,100);
+}
+
+for (int i=0; i<size; i++)
+{
+    Console.Write(array[i]+ " ");
+}
+*/
