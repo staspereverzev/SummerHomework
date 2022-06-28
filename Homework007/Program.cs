c332cdaf45ad2cf411bd1ef9007c3254b5a855aa
stas.pereverzev <stas.pereverzev@internet.ru>
2022-06-29 02:11:48 +0300

    finished

--- Homework007/Program.cs
+++ Homework007/Program.cs
@@ -1,2 +1,122 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Первая задача
+/*
+double[,] CreateRandomTwoDemArray(int a, int b, int min, int max)
+{
+    double[,] newMatrix= new double [a,b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j=0; j < b; j++)
+        {
+            newMatrix[i,j] = new Random().NextDouble() * 100;
+            Console.Write(newMatrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return newMatrix;
+
+}
+
+Console.WriteLine("Введите количество строк");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = CreateRandomTwoDemArray(a, b,1,10);
+*/
+
+// Вторая задача
+
+/*
+int[,] CreateRandomTwoDemArray(int a, int b, int min, int max)
+{
+    int[,] newMatrix= new int [a,b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j=0; j < b; j++)
+        {
+            newMatrix[i,j] = new Random().Next(min, max +1);
+            Console.Write(newMatrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return newMatrix;
+
+}
+
+Console.WriteLine("Введите количество строк");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] array = CreateRandomTwoDemArray(a, b, 1, 10);
+
+Console.WriteLine();
+
+Console.WriteLine("Введите позицию элемента (строка)");
+int aX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите позицию элемента (столбец)");
+int bX = Convert.ToInt32(Console.ReadLine());
+
+if (aX > a || bX > b)
+{
+    Console.WriteLine("Такого элемента нету");
+}
+else
+{
+    Console.WriteLine("Значение элемента = " + array[aX,bX]);
+}
+*/
+
+// Третья задача
+
+int[,] CreateRandomTwoDemArray(int a, int b, int min, int max)
+{
+    int[,] newMatrix= new int [a,b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j=0; j < b; j++)
+        {
+            newMatrix[i,j] = new Random().Next(min, max +1);
+            Console.Write(newMatrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return newMatrix;
+}
+
+Console.WriteLine("Введите количество строк");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] array = CreateRandomTwoDemArray(a, b, 1, 10);
+
+double sum = 0;
+
+Console.WriteLine();
+
+for (int j = 0; j <b; j++)
+{
+    sum = 0;
+    for (int i = 0; i < a; i++)
+    {
+        sum = array[i,j] + sum; 
+    }
+    Console.Write(sum/a + " ");
+}
+
+
